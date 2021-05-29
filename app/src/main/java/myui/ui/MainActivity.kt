1e79a6ac7e331f0f484639f41adc2b25d0b80e49
Kyant2021 <kyant2021@outlook.com>
2021-05-29 09:51:44 +0800

    Theme color can be modified

--- app/src/main/java/myui/ui/MainActivity.kt
+++ app/src/main/java/myui/ui/MainActivity.kt
@@ -20,13 +20,8 @@ import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -34,6 +29,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 import myui.ui.monet.ColorScheme
@@ -48,7 +44,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MYUITheme {
-                val themeColor = Color.Blue
+                var themeColorText by remember {
+                    mutableStateOf(TextFieldValue(MonetColor(Color.Blue.toArgb()).hex.drop(1)))
+                }
+                val themeColor = Color(themeColorText.text.toLong(16))
                 val colorScheme = ColorScheme(themeColor.toArgb(), false)
                 Log.e(
                     "ColorScheme nc",
@@ -68,15 +67,25 @@ class MainActivity : ComponentActivity() {
                             Modifier.fillMaxWidth(),
                             backgroundColor = themeColor
                         ) {
-                            Box(
-                                Modifier.padding(32.dp)
+                            Column(
+                                Modifier.padding(32.dp),
+                                horizontalAlignment = Alignment.CenterHorizontally
                             ) {
-                                Text(
-                                    "Monet Color System",
-                                    Modifier.align(Alignment.Center),
-                                    color = if (themeColor.luminance() <= 0.5f) Color.White else Color.Black,
-                                    style = MaterialTheme.typography.h4
-                                )
+                                CompositionLocalProvider(LocalContentColor provides if (themeColor.luminance() <= 0.5f) Color.White else Color.Black) {
+                                    Text(
+                                        "Monet Color System",
+                                        style = MaterialTheme.typography.h4
+                                    )
+                                    TextField(
+                                        themeColorText,
+                                        { themeColorText = it },
+                                        label = {
+                                            CompositionLocalProvider(LocalContentColor provides if (themeColor.luminance() <= 0.5f) Color.White else Color.Black) {
+                                                Text("Theme color")
+                                            }
+                                        }
+                                    )
+                                }
                             }
                         }
 
@@ -86,114 +95,49 @@ class MainActivity : ComponentActivity() {
                             contentPadding = PaddingValues(16.dp)
                         ) {
                             item { TextButton("N-1") }
-                            itemsIndexed(
-                                listOf(
-                                    Color(0xfff1effb),
-                                    Color(0xffe3e1ec),
-                                    Color(0xffc7c5cf),
-                                    Color(0xffabaab5),
-                                    Color(0xff90909a),
-                                    Color(0xff76757e),
-                                    Color(0xff5e5d67),
-                                    Color(0xff46464f),
-                                    Color(0xff2f3038),
-                                    Color(0xff1b1b21),
-                                    Color(0xff000002)
+                            itemsIndexed(colorScheme.allNeutralColors.subList(0, 11)) { i, color ->
+                                TextButton(
+                                    (if (i == 0) 50 else i * 100).toString(),
+                                    Color("ff${MonetColor(color).hex.drop(1)}".toLong(16))
                                 )
-                            ) { i, color ->
-                                TextButton((if (i == 0) 50 else i * 100).toString(), color)
                             }
                             items(4) { Spacer(Modifier.height(48.dp)) }
 
                             item { TextButton("N-2") }
-                            itemsIndexed(
-                                listOf(
-                                    Color(0xfff1f0f5),
-                                    Color(0xffe3e2e7),
-                                    Color(0xffc7c6ca),
-                                    Color(0xffababaf),
-                                    Color(0xff919095),
-                                    Color(0xff76757a),
-                                    Color(0xff5e5e62),
-                                    Color(0xff46464a),
-                                    Color(0xff303034),
-                                    Color(0xff1b1b1f),
-                                    Color(0xff000000)
+                            itemsIndexed(colorScheme.allNeutralColors.subList(11, 22)) { i, color ->
+                                TextButton(
+                                    (if (i == 0) 50 else i * 100).toString(),
+                                    Color("ff${MonetColor(color).hex.drop(1)}".toLong(16))
                                 )
-                            ) { i, color ->
-                                TextButton((if (i == 0) 50 else i * 100).toString(), color)
                             }
                             items(4) { Spacer(Modifier.height(48.dp)) }
 
                             item { TextButton("A-1") }
-                            itemsIndexed(
-                                listOf(
-                                    Color(0xffeaf1ff),
-                                    Color(0xffdbe1ff),
-                                    Color(0xffbcc2ff),
-                                    Color(0xff9da4fd),
-                                    Color(0xff8389e0),
-                                    Color(0xff686ec3),
-                                    Color(0xff5056a9),
-                                    Color(0xff383e8f),
-                                    Color(0xff202578),
-                                    Color(0xff060864),
-                                    Color(0xff000000)
+                            itemsIndexed(colorScheme.allAccentColors.subList(0, 11)) { i, color ->
+                                TextButton(
+                                    (if (i == 0) 50 else i * 100).toString(),
+                                    Color("ff${MonetColor(color).hex.drop(1)}".toLong(16))
                                 )
-                            ) { i, color ->
-                                TextButton((if (i == 0) 50 else i * 100).toString(), color)
                             }
                             items(4) { Spacer(Modifier.height(48.dp)) }
 
                             item { TextButton("A-2") }
-                            itemsIndexed(
-                                listOf(
-                                    Color(0xfff0efff),
-                                    Color(0xffe0e0fb),
-                                    Color(0xffc4c4de),
-                                    Color(0xffa9a9c2),
-                                    Color(0xff8e8fa6),
-                                    Color(0xff74748b),
-                                    Color(0xff5c5d72),
-                                    Color(0xff44455a),
-                                    Color(0xff2d2f42),
-                                    Color(0xff191a2c),
-                                    Color(0xff00000b)
+                            itemsIndexed(colorScheme.allAccentColors.subList(11, 22)) { i, color ->
+                                TextButton(
+                                    (if (i == 0) 50 else i * 100).toString(),
+                                    Color("ff${MonetColor(color).hex.drop(1)}".toLong(16))
                                 )
-                            ) { i, color ->
-                                TextButton((if (i == 0) 50 else i * 100).toString(), color)
                             }
                             items(4) { Spacer(Modifier.height(48.dp)) }
 
                             item { TextButton("A-3") }
-                            itemsIndexed(
-                                listOf(
-                                    Color(0xffffebff),
-                                    Color(0xffffd6fd),
-                                    Color(0xfff2b4db),
-                                    Color(0xffd499bf),
-                                    Color(0xffb880a3),
-                                    Color(0xff9a6688),
-                                    Color(0xff804e70),
-                                    Color(0xff663757),
-                                    Color(0xff4c2140),
-                                    Color(0xff330c29),
-                                    Color(0xff020004)
+                            itemsIndexed(colorScheme.allAccentColors.subList(22, 33)) { i, color ->
+                                TextButton(
+                                    (if (i == 0) 50 else i * 100).toString(),
+                                    Color("ff${MonetColor(color).hex.drop(1)}".toLong(16))
                                 )
-                            ) { i, color ->
-                                TextButton((if (i == 0) 50 else i * 100).toString(), color)
                             }
                             items(4) { Spacer(Modifier.height(48.dp)) }
-
-                            items(3) {
-                                TextButton("%", Color(0xFFD1E5C5))
-                            }
-                            items(15) {
-                                TextButton("1")
-                            }
-                            item {
-                                TextButton("=", Color(0xFFBAF39D))
-                            }
                         }
                     }
                 }
